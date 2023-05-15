cc9aff800fb40a552be1bbc1d375953124a1b97e
Dyego Henrique <52452813+Boo3d@users.noreply.github.com>
2023-05-15 01:45:56 -0300

    Changes (#3)
    * Fix: --global-secondary-indexes.
    
    * Re-added put-item and batch-write-itens commands.
    
    * Added: .bat files for Windows. Changed: Table is not about music anymore. Fix: Minor fixes.

---
+++ dbQuery.bat
@@ -0,0 +1,23 @@
+@echo off
+
+echo "Pesquisar item por artista"
+
+aws dynamodb query ^
+    --table-name Users ^
+    --key-condition-expression "FullName = :fullname" ^
+    --expression-attribute-values "{\":fullname\":{\"S\":\"Juliana\"}}"
+
+echo "Pesquisar item por nome e nome de usuario"
+
+aws dynamodb query ^
+    --table-name Users ^
+    --key-condition-expression "FullName = :fullname and UserName = :username" ^
+    --expression-attribute-values file://./src/keyConditions.json
+
+echo "Pesquisa pelo index secundário baseado no Email do usuário"
+
+aws dynamodb query ^
+    --table-name Users ^
+    --index-name Email-index ^
+    --key-condition-expression "Email = :email" ^
+    --expression-attribute-values "{\":email\":{\"S\":\"rafael555@email.com\"}}"
